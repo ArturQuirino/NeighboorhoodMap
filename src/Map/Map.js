b4046cf5dbdfa84c6265fa7f9b7db78699f2d659
Artur Quirino <arturquirino6@gmail.com>
2019-02-09 11:48:24 -0200

    Get map from google maps
    A google map credential was created and the map was rendered on screen.
    To facilitate, a npm package was used.
    A constants file was create, but edited to not commit the credentials.

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-export class Map extends Component {
+import Constants from '../Shared/Constants';
+ 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: -18.2404174,
+      lng: -43.6059877
+    },
+    zoom: 14.78
+  };
+ 
   render() {
     return (
-      <div>
-        My Map!
+      <div style={{ height: '100%', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: Constants.api_google_maps}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
       </div>
-    )
+    );
   }
 }
-
-export default Map
+ 
+export default Map;
---
+++ src/Shared/Constants.js
@@ -0,0 +1,5 @@
+class Constants {
+    api_google_maps = "";
+}
+
+export default Constants;
