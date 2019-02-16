eb28dfc9bcfa6c04469e1c05e7ee7e059851536c
Artur Quirino <arturquirino6@gmail.com>
2019-02-16 15:11:41 -0200

    Add list of places
    Now there is also a list of places to check

--- src/SideMenu/SideMenu.js
+++ src/SideMenu/SideMenu.js
@@ -1,58 +1,82 @@
-import React, { Component } from 'react';
-import { PropTypes } from 'prop-types';
-import AutoComplete from 'react-autocomplete';
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import AutoComplete from "react-autocomplete";
+import { List, ListItem, ListItemText } from "@material-ui/core";
 
 export class SideMenu extends Component {
+  static props = {
+    myLocations: PropTypes.array,
+    filterLocations: PropTypes.func
+  };
 
+  state = {
+    value: ""
+  };
 
-    static props = {
-        myLocations: PropTypes.array,
-        filterLocations: PropTypes.func
-    }
+  static defaultProps = {
+    myLocations: []
+  };
 
-    state = {
-        value: '',
-    }
+  handleListItemClick = (name) => {
+    this.props.filterLocations(name)
+  }
 
-    static defaultProps = {
-        myLocations: []
-    };
-
-    render() {
-        return (
-        <div style={sideMenuContainerStyle}>
-            <AutoComplete 
-                items={this.props.myLocations}
-                shouldItemRender={(item,value) => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1}
-                getItemValue={item => item.name}
-                renderItem={(item, highlighted) => 
-                    <div
-                    key={item.name}
-                    style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
-                >
-                    {item.name}
-                </div>
-                }
-                value={this.state.value}
-                onChange={e => {
-                    this.setState({ value: e.target.value });
-                    this.props.filterLocations(e.target.value);
+  render() {
+    return (
+      <div style={sideMenuContainerStyle}>
+        <div style={autoCompleteStyle}>
+          <AutoComplete
+            items={this.props.myLocations}
+            shouldItemRender={(item, value) =>
+              item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+            }
+            getItemValue={item => item.name}
+            renderItem={(item, highlighted) => (
+              <div
+                key={item.name}
+                style={{
+                  backgroundColor: highlighted ? "#eee" : "transparent"
                 }}
-                onSelect={value => {
-                    this.props.filterLocations(value);
-                    this.setState({value})
-                }}
-            />
+              >
+                {item.name}
+              </div>
+            )}
+            value={this.state.value}
+            onChange={e => {
+              this.setState({ value: e.target.value });
+              this.props.filterLocations(e.target.value);
+            }}
+            onSelect={value => {
+              this.props.filterLocations(value);
+              this.setState({ value });
+            }}
+          />
         </div>
-        )
-    }
+
+        <List>
+          {this.props.myLocations.map(loc => (
+            <ListItem button key={loc.id} onClick={() => this.handleListItemClick(loc.name)}>
+              <ListItemText primary={loc.name} />
+            </ListItem>
+          ))}
+          <ListItem button key='-1' onClick={() => this.handleListItemClick('')}>
+            <ListItemText primary='All Places' />
+        </ListItem>
+        </List>
+      </div>
+    );
+  }
 }
 
 const sideMenuContainerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight:  '100%',
-    padding: '20px'
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "100%",
+  padding: "20px"
+};
+
+const autoCompleteStyle = {
+  margin: "0 0 30% 0"
 };
 
 export default SideMenu;
